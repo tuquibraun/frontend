4d0184c4624b2e1c67d62e9f3847edcfb945d5cf
Tuqui Braun <tuquibraun@gmail.com>
2019-05-27 15:20:45 -0300

    rutas

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,12 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuard } from '../guards/auth.guard';
 import { AdminGuard } from '../guards/admin.guard';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
+import { AdminMembershipsComponent } from './admin-memberships/admin-memberships.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
-    canActivate: [AuthGuard, AdminGuard]
+    canActivate: [AuthGuard, AdminGuard],
+    children: [
+      {
+        path: '',
+        children: [
+          { path: 'members', component: AdminUsersComponent },
+          { path: 'membership', component: AdminMembershipsComponent },
+          { path: 'dashboard', component: DashboardComponent },
+          { path: '', component: DashboardComponent }
+        ]
+      }
+    ]
   }
 ];
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './session/login/login.component';
+import { PassRecoveryComponent } from './session/pass-recovery/pass-recovery.component';
+import { RegisterComponent } from './session/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'pass-recovery',
+    component: PassRecoveryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
